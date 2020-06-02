9f2885cb30db0a598ff75e43a2cb85a053c98a4a
Justin Pilapil <57848030+jpilapil@users.noreply.github.com>
2020-06-02 10:59:50 -0700

    edit functions

--- functions.js
+++ functions.js
@@ -1,49 +1,3 @@
-// Your functions go here!
-
-// USE DESTRUCTURING ASSIGNMENT TO ASSIGN VARIABLES FROM OBJECTS-----
-// const HIGH_TEMPERATURES = {
-//    yesterday: 75,
-//    today: 77,
-//    tomorrow: 80,
-// };
-// // take the value of the today key
-// // assign value to a new key, highToday
-// // push highToday to object HIGH_TEMP
-// const { today: highToday } = HIGH_TEMPERATURES;
-// const { tomorrow: highTomorrow } = HIGH_TEMPERATURES;
-
-// // USE DECONSTRUCTURING ASSIGNMENT TO ASSIGN VARIABLES FROM NESTED OBJECT-----
-// const LOCAL_FORECAST = {
-//    yesterday: { low: 61, high: 75 },
-//    today: { low: 64, high: 77 },
-//    tomorrow: { low: 68, high: 80 },
-// };
-// // target the key of today in object LOCAL_FORCAST
-// // target the value of the key high and push the same value into the key highToday
-// // push highToday to LOCAL_FORECAST object
-// const {
-//    today: { high: highToday },
-// } = LOCAL_FORECAST;
-// const {
-//    today: { low: lowToday },
-// } = LOCAL_FORECAST;
-
-// // FUNCTIONAL FUNCTION SYNTAX
-
-// // old way of creating function
-// function getPrice(subTotal, shipping) {
-//    return subTotal + shipping;
-// }
-// // new way of creating function
-// // getPrice declared with let or const
-// // getPrice = the parameters the function takes
-// // => meaning it makes it a function
-// // returns the given parameters
-
-// const getPrice = (subTotal, shipping) => {
-//    return subTotal + shipping;
-// };
-
 // IMPLEMENT THE FILTER METHOD ON A PROTOTYPE ------
 
 // more info on callback
@@ -62,7 +16,6 @@ function filterMethod(num) {
          if (callback(arr[index]))
             // calls the function on line 68 with callback and passes in each index of the array
             newArray.push(arr[index]); // pushes the result of the callback into the empty newArray -- if the number has remainder of 1, push to newArray
-
       return newArray; // returns new populated array
    };
    var inputArr = arr.myFilter(function (item) {
